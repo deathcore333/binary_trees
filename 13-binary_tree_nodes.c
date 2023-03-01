063035c9cc7d6c89ba5fe20a3c3a16497ca2ff57
deathcore333 <amossmwangii@gmail.com>
2023-03-01 23:14:51 +0300

    new changes

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -8,15 +8,13 @@
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	size_t count = 0;
-	if (!tree)
-		return (0);
+	size_t nodes = 0;
 
-	if (tree->left || tree->right)
-		count = 1;
-
-	count += binary_tree_nodes(tree->left);
-	count += binary_tree_nodes(tree->right);
-
-	return (count);
+	if (tree)
+	{
+		nodes += (tree->left || tree->right) ? 1 : 0;
+		nodes += binary_tree_nodes(tree->left);
+		nodes += binary_tree_nodes(tree->right);
+	}
+	return (nodes);
 }
